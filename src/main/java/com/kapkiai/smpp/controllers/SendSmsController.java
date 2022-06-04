eb253618262cf5b9a970829a9b547d6af7c5e0b7
Mathew Kapkiai <mathewkiprop17@gmail.com>
2022-06-04 21:43:13 +0300

    Fixed root api path

--- src/main/java/com/kapkiai/smpp/controllers/SendSmsController.java
+++ src/main/java/com/kapkiai/smpp/controllers/SendSmsController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Controller
 @RestController
+@RequestMapping("/api/v1")
 public class SendSmsController {
 
     @Autowired
